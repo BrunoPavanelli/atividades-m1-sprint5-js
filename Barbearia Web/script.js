29860612bec8173362b26450b7a0844d4d3fa020
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-24 19:06:53 -0300

    atividade barbearia web finalizada

--- Barbearia Web/script.js
+++ Barbearia Web/script.js
@@ -19,29 +19,142 @@ const barbearia = {
   
   
   
-//   ⁠
-//   function buscaCortePorId(id) {
-//   }
+function buscaCortePorId(id) {
+    let buscador = barbearia.cortes
+    let resultado
+    for (let i = 0; i <buscador.length; i++) {
+        if (i === id) {
+            if (i === 0) {
+                return 'Corte não encontrado'
+            }
+            return buscador[i - 1]
+        } else {
+            resultado = 'Corte não encontrado'
+        }
+    }  
+    return resultado
+}
+console.log(`Busca por Corte`)
+console.log(buscaCortePorId(2))
   
-//   function buscaBarbaPorId(id) {
-//   }
+function buscaBarbaPorId(id) {
+    let buscador = barbearia.barbas
+    let resultado
+    for (let i = 0; i <buscador.length; i++) {
+        if (i === id) {
+            if (i === 0) {
+                return 'Barba não encontrado'
+            }
+            return buscador[i - 1]
+        } else {
+            resultado = 'Barba não encontrado'
+        }
+    }  
+    return resultado
+}
+console.log(`Busca por Barba`)
+console.log(buscaBarbaPorId(3))
   
-//   function verificaStatusBarbearia() {
-//   }
+function verificaStatusBarbearia() {
+    let buscador = barbearia.estaAberto
+    return buscador === true ?  'Estamos abertos' : 'Estamos fechados'
+}
+console.log('Verificar se Barbaearia está aberta')
+console.log(verificaStatusBarbearia())
   
-//   function retornaTodosCortes() {
-//   }
+function retornaTodosCortes() {
+    return barbearia.cortes
+}
+console.log('Todos os Cortes')
+console.log(retornaTodosCortes())
   
-//   function retornaTodasBarbas() {
-//   }
+function retornaTodasBarbas() {
+    return barbearia.barbas
+}
+console.log('Todos as Barbas')
+console.log(retornaTodasBarbas())
   
-//   function criaPedido(nomeCliente, corteId, barbaId) {
-//   }
+function criaPedido(nomeCliente, corteId, barbaId) {
+    //verficar se os id's são válidos:
+        if (corteId === 0) {
+            return 'id de Corte Inválido'
+        } else if (corteId > barbearia.cortes.length) {
+            return 'id de Corte Inválido'
+        }     
+        if (barbaId === 0) {
+            return 'id de Barba Inválido'  
+        }  else if (barbaId > barbearia.cortes.length) {
+            return 'id de Barba Inválido'
+        } 
+        
+        let pedidoCorte =  buscaCortePorId(corteId) 
+        let pedidoBarba =  buscaBarbaPorId(barbaId)
+        
+        if (corteId === undefined && barbaId === undefined) {
+            return 'É necessário escolher ao menos um Corte ou Barba'
+        }
+
+        let pedido = {}
+        if (barbaId === undefined) {
+            pedido = {
+                nome: nomeCliente,
+                pedidoCorteTipo: pedidoCorte.tipo,
+                pedidoCortePreco: pedidoCorte.valor,
+            }
+        } else if (corteId === undefined) {
+            pedido = {
+                nome: nomeCliente,
+                pedidoBarbaTipo: pedidoBarba.tipo,
+                pedidoBarbaPreco: pedidoBarba.valor
+            }
+        } else {
+            pedido = {
+                nome: nomeCliente,
+                pedidoCorteTipo: pedidoCorte.tipo,
+                pedidoCortePreco: pedidoCorte.valor,
+                pedidoBarbaTipo: pedidoBarba.tipo,
+                pedidoBarbaPreco: pedidoBarba.valor
+            }
+
+    }
+        return pedido
+}
+console.log('Pedido escolhido')
+console.log(criaPedido('Bruno', 3, 3))
   
-//   function atualizaPedido(lista, id, preco, tipo) {
-//   }
-  
-//   function calculaTotal(pedido) {
-//   }
-  
-  
+function atualizaPedido(lista, id, novoPreco, novoTipo) {
+    if (id === 0 || id > barbearia.cortes.length || id > barbearia.barbas.length || (novoPreco === undefined && novoTipo === undefined)) {
+        return 'Entrada de valores inválidos'
+    }
+
+    if (novoPreco === undefined) {
+        lista[id - 1].tipo = novoTipo
+    } else if (novoTipo === undefined) {
+        lista[id - 1].valor = novoPreco
+    } else {
+        lista[id - 1].tipo = novoTipo
+        lista[id - 1].valor = novoPreco
+    }
+    console.log(lista[id - 1])
+    return 'Item atualizaco com sucesso'
+}
+console.log('Item de lista atualizado')
+console.log(atualizaPedido(barbearia.cortes, 1, 30, 'Hawaino'))
+
+let orcamento = criaPedido('Bruno', 3 ,3)
+function calculaTotal(pedido) {
+    let somaPreco = 0
+    if (pedido === 'id de Corte Inválido') {
+        return 'Pedido Inválido'
+    }
+    if (pedido.pedidoBarbaPreco === undefined) {
+        return pedido.pedidoCortePreco
+    } else if (pedido.pedidoCortePreco === undefined) {
+        return pedido.pedidoBarbaPreco
+    } else {
+        somaPreco = pedido.pedidoBarbaPreco + pedido.pedidoCortePreco
+    }
+    return `${pedido.nome}. O valor total do seu pedido foi de R$${somaPreco.toFixed(1)}.`
+}
+console.log('Mostrar valor total do pedido')
+console.log(calculaTotal(orcamento))
