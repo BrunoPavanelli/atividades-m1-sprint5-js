1cab676ccd366259b592910124d60d5c40cd6a88
Bruno Pavanelli <brunopavanellicontato@gmail.com>
2022-10-24 17:54:20 -0300

    iniciando atividade: barbearia web. arquivos prontos para manuseio

---
+++ Barbearia Web/script.js
@@ -0,0 +1,47 @@
+const barbearia = {
+    cortes: [
+      { id: 1, tipo: "Militar", valor: 20.0 },
+      { id: 2, tipo: "Samurai", valor: 35.0 },
+      { id: 3, tipo: "Pompadour", valor: 20 },
+      { id: 4, tipo: "Moicano", valor: 0 },
+      { id: 5, tipo: "Razor part", valor: 0 },
+    ],
+  
+    barbas: [
+      { id: 1, tipo: "Capitão Jack", valor: 20.0 },
+      { id: 2, tipo: "Van Dyke", valor: 20.0 },
+      { id: 3, tipo: "Barba Média", valor: 20.0 },
+      { id: 4, tipo: "Barba Baixa", valor: 20.0 },
+      { id: 5, tipo: "Barba Zero", valor: 15.0 },
+    ],
+    estaAberto: true,
+  };
+  
+  
+  
+//   ⁠
+//   function buscaCortePorId(id) {
+//   }
+  
+//   function buscaBarbaPorId(id) {
+//   }
+  
+//   function verificaStatusBarbearia() {
+//   }
+  
+//   function retornaTodosCortes() {
+//   }
+  
+//   function retornaTodasBarbas() {
+//   }
+  
+//   function criaPedido(nomeCliente, corteId, barbaId) {
+//   }
+  
+//   function atualizaPedido(lista, id, preco, tipo) {
+//   }
+  
+//   function calculaTotal(pedido) {
+//   }
+  
+  
